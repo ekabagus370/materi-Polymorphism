310c961bbca7d85b1a658063e10f0f6991796785
ekabagus370 <ekabaguserlangga370@amikom.ac.id>
2022-06-13 00:55:23 +0700

    materi Polymorphism

---
+++ Eka Bagus Erlangga_21.11.4287_Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih printer:");
+            Console.WriteLine("1. Canon");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("1. LaserJet\n");
+
+            Console.Write("Nomor printer [1..3]: ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+            {
+                printer = new Canon();
+            }
+            else if (nomorPrinter == 2)
+            {
+                printer = new Epson();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
